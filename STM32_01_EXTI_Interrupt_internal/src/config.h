2ff748e70645e52a7219c2c110f949f33ee24f2a
faisaljanuzaj578-lang <faisaljanuzaj578@gmail.com>
2026-03-09 06:57:20 +0700

    Add files via upload

---
+++ STM32_01_EXTI_Interrupt_internal/src/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// ================= OLED CONFIG =================
+#define OLED_WIDTH     128
+#define OLED_HEIGHT    64
+#define OLED_ADDRESS   0x3C
+
+// Pin I2C STM32F103
+#define OLED_SDA       PB7
+#define OLED_SCL       PB6
+
+#endif
